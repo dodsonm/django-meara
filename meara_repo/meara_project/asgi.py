0f441bd59cef62872284caa355d156fb587f7521
dodson <matthew.dodson.dc@gmail.com>
2021-11-15 11:13:13 -0500

    FIX Finish renaming meara_repo to meara_project

--- meara_repo/meara_project/asgi.py
+++ meara_repo/meara_project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for meara_repo project.
+ASGI config for meara_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meara_repo.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meara_project.settings')
 
 application = get_asgi_application()
--- meara_repo/meara_project/wsgi.py
+++ meara_repo/meara_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for meara_repo project.
+WSGI config for meara_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meara_repo.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meara_project.settings')
 
 application = get_wsgi_application()
