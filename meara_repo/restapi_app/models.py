8deeaa901da16e7fe4edf5d002dd824e731c5a1f
dodson <matthew.dodson.dc@gmail.com>
2021-11-15 11:15:19 -0500

    Start adding models to restapi

--- meara_repo/restapi_app/models.py
+++ meara_repo/restapi_app/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+from django.db import models as m
 
 # Create your models here.
+
+class System(m.Model):
+    name = m.CharField(max_length=128)
+    created = m.DateTimeField('Date/Time created')
+
+class Organization(m.Model):
+    name = m.CharField(max_length=128)
+    created = m.DateTimeField('Date/Time created')
+
+class Application(m.Model):
+    name = m.CharField(max_length=128)
+    created = m.DateTimeField('Date/Time created')
